74724ce4851cc267646af236eeb911ea736ca8b7
lzyyauto <lzyyauto@gmail.com>
2024-07-24 18:14:56 +0800

    增加手动维护开关

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  devServer: {
+    setupMiddlewares: (middlewares, devServer) => {
+      const MAINTENANCE_MODE = true; //手动维护
+
+      if (MAINTENANCE_MODE) {
+        devServer.app.get('*', (req, res) => {
+          res.sendFile(path.resolve(__dirname, 'public', 'maintenance.html'));
+        });
+      }
+
+      return middlewares;
+    }
+  }
+});
